7347353a330d76666d7b163b2a999c0039f970f0
Oliver Muir <ojcmuir@gmail.com>
2019-06-07 08:25:42 +0100

    Add new and create routes

--- app.js
+++ app.js
@@ -8,9 +8,27 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 
-app.get("*", function(req, res){
-    console.log("Render landing")
+app.get("/", function(req, res){
+    console.log("Render landing");
     res.render("landing");
+});
+
+// INDEX route
+app.get("/reviews", function(req, res){
+    res.send("REVIEWS");
+});
+
+// NEW route
+app.get("/reviews/new", function(req, res){
+    res.send("NEW FORM");
+});
+
+// CREATE route
+app.post("/reviews", function(req, res){
+    // store new review in DB
+    
+    // show all reviews
+    res.redirect("/reviews");
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
