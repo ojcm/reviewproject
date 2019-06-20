92c438254fda964b7dc77001ebd06cf4a0c35865
oliver <ojcmuir@gmail.com>
2019-06-20 15:59:52 +0000

    Disable register and login routes

--- routes/index.js
+++ routes/index.js
@@ -6,44 +6,44 @@ var express = require("express"),
 //==========================
 // AUTH ROUTES
 //==========================
-router.get("/register", function(req, res) {
-    res.render("register");
-});
+// router.get("/register", function(req, res) {
+//     res.render("register");
+// });
 
-router.post("/register", function(req, res){
-    var newUser = new User({username: req.body.username});
-    User.register(newUser, req.body.password, function(err, user){
-        if (err) {
-            console.log(err);
-            return res.render("register");
-        }
-        passport.authenticate("local")(req, res, function(){
-            res.redirect("/companies");
-        });
-    });
-});
+// router.post("/register", function(req, res){
+//     var newUser = new User({username: req.body.username});
+//     User.register(newUser, req.body.password, function(err, user){
+//         if (err) {
+//             console.log(err);
+//             return res.render("register");
+//         }
+//         passport.authenticate("local")(req, res, function(){
+//             res.redirect("/companies");
+//         });
+//     });
+// });
 
-router.get("/login", function(req, res) {
-    res.render("login");
-});
+// router.get("/login", function(req, res) {
+//     res.render("login");
+// });
 
-router.post("/login", passport.authenticate("local", 
-    {
-        successRedirect:"/companies", 
-        failureRedirect:"/login"
-    }), function(req, res) {
-});
+// router.post("/login", passport.authenticate("local", 
+//     {
+//         successRedirect:"/companies", 
+//         failureRedirect:"/login"
+//     }), function(req, res) {
+// });
 
-router.get("/logout", function(req, res) {
-    req.logout();
-    res.redirect("/companies");
-});
+// router.get("/logout", function(req, res) {
+//     req.logout();
+//     res.redirect("/companies");
+// });
 
 //==========================
 // PROFILE ROUTES
 //==========================
-router.get("/profile", function(req, res){
-    res.render("profile");
-});
+// router.get("/profile", function(req, res){
+//     res.render("profile");
+// });
 
 module.exports = router;
